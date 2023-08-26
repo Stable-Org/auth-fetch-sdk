cde7681710ed6e7d242cd17e61b05b2e29dd6f12
jonURT <123364219+jonURT@users.noreply.github.com>
2023-08-26 08:54:01 +0000

    Update SDK

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,11 @@
 /* tslint:disable */
 /* eslint-disable */
+export * from './AcceptedResponseDto';
 export * from './BadRequestResponseDto';
-export * from './LoginUserRequestDto';
-export * from './LoginUserResponseDto';
+export * from './ConfirmRequestDto';
+export * from './LoginRequestDto';
+export * from './LoginResponseDto';
+export * from './RefreshTokenRequestDto';
+export * from './RefreshTokenResponseDto';
+export * from './RegisterRequestDto';
 export * from './UnauthorizedResponseDto';
